c7a11c9481059a3df94fd43042856f51140088fc
graceA <gracemarycanto@gmail.com>
2026-06-29 10:40:32 +0530

    edited yml for debugging

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,6 +1,7 @@
 package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(features = {"src/test/resources/features"},
@@ -10,6 +11,11 @@ import io.cucumber.testng.CucumberOptions;
     )
 
     public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 
 
 
